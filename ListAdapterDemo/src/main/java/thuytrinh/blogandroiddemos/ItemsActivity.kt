4241fa688268854eba6ae5edb4d4d6e76863d70e
Thuy Trinh <thuy.trinh@cobi.bike>
2019-09-12 12:46:44 +0200

    Add RecyclerView library & rename into ItemsActivity

--- ListAdapterDemo/src/main/java/thuytrinh/blogandroiddemos/ItemsActivity.kt
+++ ListAdapterDemo/src/main/java/thuytrinh/blogandroiddemos/ItemsActivity.kt
@@ -3,9 +3,9 @@ package thuytrinh.blogandroiddemos
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ItemsActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_items)
   }
 }
