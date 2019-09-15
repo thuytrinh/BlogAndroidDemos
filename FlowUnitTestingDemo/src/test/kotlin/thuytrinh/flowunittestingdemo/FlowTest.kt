11c96dbde7f938aa21912af6d1fa0dd90be3ef6d
Thuy Trinh <thuy.trinh@cobi.bike>
2019-09-15 23:52:29 +0200

    Write first 2 tests

--- FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/FlowTest.kt
+++ FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/FlowTest.kt
@@ -1,10 +1,26 @@
 package thuytrinh.flowunittestingdemo
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.jupiter.api.Test
 
 class FlowTest {
   @Test
-  fun test() {
-    println("Hello Flow<T>!")
+  fun `launch with GlobalScope`() {
+    GlobalScope.launch {
+      flowOf(0, 1, 2, 3).collect { println(it) }
+    }
+    println("Done.")
+  }
+
+  @Test
+  fun `launch with runBlockingTest`() {
+    runBlockingTest {
+      flowOf(0, 1, 2, 3).collect { println(it) }
+    }
+    println("Done.")
   }
 }
