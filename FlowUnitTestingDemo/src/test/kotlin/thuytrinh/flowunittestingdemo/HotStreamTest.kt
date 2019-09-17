29d4fff3ab5fedf3a49c60c0c965e224816c454d
Thuy Trinh <thuy.trinh@cobi.bike>
2019-09-18 00:40:47 +0200

    Remove repeat

--- FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/HotStreamTest.kt
+++ FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/HotStreamTest.kt
@@ -28,47 +28,43 @@ class HotStreamTest {
 
   @Test
   fun `should work with callbackFlow`() = runBlockingTest {
-    repeat(10_000) {
-      val broadcastManager = LocalBroadcastManager()
-      val values = mutableListOf<Int>()
-      val job = launch {
-        broadcastManager.asFlow().collect { values.add(it) }
-      }
-
-      broadcastManager.sendBroadcast(0)
-      expectThat(values).contains(0)
-
-      broadcastManager.sendBroadcast(1)
-      expectThat(values).contains(0, 1)
-
-      broadcastManager.sendBroadcast(2)
-      expectThat(values).contains(0, 1, 2)
-
-      job.cancel()
+    val broadcastManager = LocalBroadcastManager()
+    val values = mutableListOf<Int>()
+    val job = launch {
+      broadcastManager.asFlow().collect { values.add(it) }
     }
+
+    broadcastManager.sendBroadcast(0)
+    expectThat(values).contains(0)
+
+    broadcastManager.sendBroadcast(1)
+    expectThat(values).contains(0, 1)
+
+    broadcastManager.sendBroadcast(2)
+    expectThat(values).contains(0, 1, 2)
+
+    job.cancel()
   }
 
   @Test
   fun `should work with ConflatedBroadcastChannel`() = runBlockingTest {
-    repeat(10_000) {
-      val publisher = ConflatedBroadcastChannel(0)
-      val values = mutableListOf<Int>()
+    val publisher = ConflatedBroadcastChannel(0)
+    val values = mutableListOf<Int>()
 
-      val job = launch {
-        publisher.asFlow().collect { values.add(it) }
-      }
-      expectThat(publisher.value).isEqualTo(0)
-
-      publisher.offer(1)
-      expectThat(values).contains(0, 1)
-      expectThat(publisher.value).isEqualTo(1)
-
-      publisher.offer(2)
-      expectThat(values).contains(0, 1, 2)
-      expectThat(publisher.value).isEqualTo(2)
-
-      job.cancel()
+    val job = launch {
+      publisher.asFlow().collect { values.add(it) }
     }
+    expectThat(publisher.value).isEqualTo(0)
+
+    publisher.offer(1)
+    expectThat(values).contains(0, 1)
+    expectThat(publisher.value).isEqualTo(1)
+
+    publisher.offer(2)
+    expectThat(values).contains(0, 1, 2)
+    expectThat(publisher.value).isEqualTo(2)
+
+    job.cancel()
   }
 }
 
