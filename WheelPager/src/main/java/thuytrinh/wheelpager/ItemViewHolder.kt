4f28f6e9a756158ea7d9928c17cf99995df1add8
Thuy Trinh <thuy.trinh@cobi.bike>
2019-09-22 22:31:57 +0200

    Add WheelPager

---
+++ WheelPager/src/main/java/thuytrinh/wheelpager/ItemViewHolder.kt
@@ -0,0 +1,22 @@
+package thuytrinh.wheelpager
+
+import android.graphics.Color
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+  private val textView: TextView = itemView.findViewById(R.id.textView)
+
+  fun bindTo(item: Int) {
+    textView.text = item.toString()
+    val backgroundColor = when {
+      item % 2 == 0 -> Color.parseColor("#909090")
+      else -> Color.parseColor("#D0D0D0")
+    }
+    itemView.setBackgroundColor(backgroundColor)
+
+    // Just for debugging purpose.
+    itemView.tag = "Page #$item"
+  }
+}
---
+++ WheelPager/src/main/java/thuytrinh/wheelpager/ItemsAdapter.kt
@@ -0,0 +1,30 @@
+package thuytrinh.wheelpager
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+
+typealias Item = Int
+
+class ItemsAdapter : ListAdapter<Item, ItemViewHolder>(
+  object : DiffUtil.ItemCallback<Int>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+      return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+      return oldItem == newItem
+    }
+  }
+) {
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+    val itemView = LayoutInflater.from(parent.context)
+      .inflate(R.layout.item, parent, false)
+    return ItemViewHolder(itemView)
+  }
+
+  override fun onBindViewHolder(holder: ItemViewHolder, position: Item) {
+    holder.bindTo(getItem(position))
+  }
+}
