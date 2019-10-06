8c5b324368741db0bca52df1fa00fedce9aa5ef9
Thuy Trinh <thuy.trinh@cobi.bike>
2019-10-06 19:12:05 +0200

    Add data binding library & show hotels

--- HotelsApp/HotelList/src/main/java/thuytrinh/hotelsapp/HotelListFragment.kt
+++ HotelsApp/HotelList/src/main/java/thuytrinh/hotelsapp/HotelListFragment.kt
@@ -1,10 +1,20 @@
 package thuytrinh.hotelsapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import thuytrinh.hotelsapp.hotellist.databinding.HotelListBinding
 
 class HotelListFragment : Fragment() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    return HotelListBinding.inflate(inflater, container, false).run {
+      root
+    }
   }
 }
