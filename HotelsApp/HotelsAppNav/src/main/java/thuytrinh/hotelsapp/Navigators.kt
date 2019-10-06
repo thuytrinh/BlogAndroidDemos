073bc8c6613628e65cfdd3d5c2d6b87f6b554391
Thuy Trinh <thuy.trinh@cobi.bike>
2019-10-06 23:03:02 +0200

    Add actions

--- HotelsApp/HotelsAppNav/src/main/java/thuytrinh/hotelsapp/Navigators.kt
+++ HotelsApp/HotelsAppNav/src/main/java/thuytrinh/hotelsapp/Navigators.kt
@@ -6,13 +6,13 @@ import thuytrinh.hotelsapp.hotelsappnav.R
 
 object HotelListNavigatorImpl : HotelListNavigator {
   override fun navigateToHotelDetails(navController: NavController) {
-    navController.navigate(R.id.hotelDetailsFragment)
+    navController.navigate(R.id.fromHotelListToHotelDetails)
   }
 }
 
 object HotelDetailsNavigatorImpl : HotelDetailsNavigator {
   override fun navigateToBooking(navController: NavController) {
-    navController.navigate(R.id.hotelBookingFragment)
+    navController.navigate(R.id.fromHotelDetailsToHotelBooking)
   }
 }
 
