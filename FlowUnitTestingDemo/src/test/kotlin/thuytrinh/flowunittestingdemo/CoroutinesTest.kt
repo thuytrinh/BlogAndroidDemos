60d8a0d325490f8e83f8fa6f9aa0acbb86ab7a89
Thuy Trinh <thuy.trinh@cobi.bike>
2020-03-20 11:40:00 +0100

    Use toList()

--- FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/CoroutinesTest.kt
+++ FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/CoroutinesTest.kt
@@ -3,10 +3,16 @@ package thuytrinh.flowunittestingdemo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
 
 class CoroutinesTest {
   @Test
@@ -21,5 +27,22 @@ class CoroutinesTest {
       }
     }
     println("Calling test() is done")
+  }
+
+  @Test
+  fun `can use toList instead of collect`() = runBlockingTest {
+    // Given
+    val publisher = ConflatedBroadcastChannel(0)
+    val events = mutableListOf<Int>()
+
+    // When
+    // Not recommended:
+    // val job = launch { publisher.asFlow().collect { events.add(it) } }
+    // Recommended:
+    val job = launch { publisher.asFlow().toList(events) }
+
+    // Then
+    expectThat(events).containsExactly(0)
+    job.cancel()
   }
 }
