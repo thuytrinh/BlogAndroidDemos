604f4b366e24f03a6afec485859b20e3f89ffe79
Thuy Trinh <thuy.trinh@cobi.bike>
2020-03-23 12:40:15 +0100

    Ask permission by using the fragment library

--- FragmentContainerViewDemo/src/main/java/thuytrinh/fragmentcontainerviewdemo/HotelsFragment.kt
+++ FragmentContainerViewDemo/src/main/java/thuytrinh/fragmentcontainerviewdemo/HotelsFragment.kt
@@ -1,9 +1,12 @@
 package thuytrinh.fragmentcontainerviewdemo
 
+import android.Manifest.permission
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import thuytrinh.fragmentcontainerviewdemo.databinding.HotelsBinding
 
@@ -13,6 +16,21 @@ class HotelsFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return HotelsBinding.inflate(inflater, container, false).root
+    return HotelsBinding.inflate(inflater, container, false).apply {
+      requestButton.setOnClickListener {
+        val launcher = prepareCall(ActivityResultContracts.RequestPermissions()) { results ->
+          if (results.values.all { it }) {
+            Toast.makeText(requireContext(), "Thanks!", Toast.LENGTH_SHORT).show()
+          } else {
+            Toast.makeText(requireContext(), "Oops! (ಥ﹏ಥ)", Toast.LENGTH_SHORT).show()
+          }
+        }
+        launcher.launch(
+          arrayOf(
+            permission.ACCESS_COARSE_LOCATION
+          )
+        )
+      }
+    }.root
   }
 }
