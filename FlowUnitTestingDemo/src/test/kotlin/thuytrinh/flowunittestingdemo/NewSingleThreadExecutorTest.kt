ea1e11f6b21941c05b467160835a43fef63191cc
Thuy Trinh <thuy.trinh@cobi.bike>
2020-04-17 16:52:49 +0200

    Add Mutex test

--- FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/NewSingleThreadExecutorTest.kt
+++ FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/NewSingleThreadExecutorTest.kt
@@ -1,11 +1,16 @@
 package thuytrinh.flowunittestingdemo
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 
@@ -30,5 +35,35 @@ class NewSingleThreadExecutorTest {
     val i = scope.launch { taskA() }
     val j = scope.launch { taskB() }
     listOf(i, j).joinAll()
+  }
+
+  class ServiceA(private val mutex: Mutex) {
+    suspend fun taskA() = mutex.withLock {
+      repeat(times = 10) {
+        delay(500)
+        println("A: $it")
+      }
+    }
+  }
+
+  class ServiceB(private val mutex: Mutex) {
+    suspend fun taskB() = mutex.withLock {
+      repeat(times = 10) {
+        delay(500)
+        println("B: $it")
+      }
+    }
+  }
+
+  @Test
+  fun `should execute serially with Mutex`() = runBlocking<Unit> {
+    val sharedMutex = Mutex()
+    val serviceA = ServiceA(sharedMutex)
+    val serviceB = ServiceB(sharedMutex)
+
+    listOf(
+      async(Dispatchers.IO) { serviceA.taskA() },
+      async(Dispatchers.IO) { serviceB.taskB() }
+    ).awaitAll()
   }
 }
