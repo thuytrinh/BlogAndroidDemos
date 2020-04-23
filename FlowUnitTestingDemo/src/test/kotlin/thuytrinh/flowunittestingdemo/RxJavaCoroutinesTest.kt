006dc00e6b241fe552bff15a7342078ca78e365d
Thuy Trinh <thuy.trinh@cobi.bike>
2020-04-23 16:02:40 +0200

    Add a test to convert Single into suspend func

--- FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/RxJavaCoroutinesTest.kt
+++ FlowUnitTestingDemo/src/test/kotlin/thuytrinh/flowunittestingdemo/RxJavaCoroutinesTest.kt
@@ -1,6 +1,7 @@
 package thuytrinh.flowunittestingdemo
 
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -10,6 +11,8 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.rx2.asFlow
+import kotlinx.coroutines.rx2.await
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class RxJavaCoroutinesTest {
@@ -43,5 +46,11 @@ class RxJavaCoroutinesTest {
 
     delay(1000)
     subscription.dispose()
+  }
+
+  @Test
+  fun `should convert Single to suspend function`() = runBlocking<Unit> {
+    val value = Single.just(2).await()
+    assertThat(value).isEqualTo(2)
   }
 }
